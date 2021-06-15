0d9965d75fbb29bcf259c22850616a89d7d3373f
ShreyanshSingh20 <shreyanshsingh40@gmail.com>
2021-06-16 00:12:26 +0530

    Prediction Type Functionality Added for python

--- app.js
+++ app.js
@@ -72,10 +72,8 @@ app.post("/", function(req, resp) {
       //   (async () => {
       // const {stdout} = await subprocess;
 
-      // this please_plot is a string made by combining the plots needed by user
-      // var please_plot = "close open low"
-      var please_plot = "close"
-      const python = spawn('python', ['test.py', info, please_plot]);
+      var plotType=req.body.plot;
+      const python = spawn('python', ['test.py',info, plotType]);
       // collect data from script
       python.stdout.on('data', function(data) {
         console.log('Pipe data from python script ...');
--- test.py
+++ test.py
@@ -38,15 +38,39 @@ finally:
     from tqdm import tqdm_notebook
     from itertools import product
 
+# import warnings
+# warnings.filterwarnings('ignore')
 
-# Getting the Data in string format from NodeJS
-# NodeJS passed 2 args ['./test.py', data] where sys.argv[1] represents DATA
+
 data = sys.argv[1]
-
-# Creating a Pandas DataFrame for the supplied data 
 my_data_new = pd.DataFrame([x.split(',') for x in data.split('\n')])
+# print(my_data_new.columns)
 
-# Rearranging and Cleaning the Data 
+# my_data_list_tmp = my_data_str.split('\r')      # breaking into list
+# my_data_list_tmp.pop()
+
+# my_data_list_tmp2 = []          # this will break each list to form rows and columns
+# for st in my_data_list_tmp:
+#     my_st = st.split(',')
+
+#     my_data_list_tmp2.append(my_st);
+
+# R = len(my_data_list_tmp2)
+# C = len(my_data_list_tmp2[0])
+# for i in range(1,R):
+#     prev_text = my_data_list_tmp2[i][0]
+
+#     # replace the \n from the dates
+#     new_text = prev_text[1:]
+#     my_data_list_tmp2[i][0] = my_data_list_tmp2[i][0].replace(prev_text, new_text)
+
+#     # changing the data types of the rest columns other than dates to float
+#     for j in range(1,C):
+#         my_data_list_tmp2[i][j] = float(my_data_list_tmp2[i][j])
+
+# my_data_new = pd.DataFrame(my_data_list_tmp2)
+# # print(my_data_new.shape)
+
 my_data_new.columns = my_data_new.iloc[0]
 my_data_new = my_data_new.drop(columns = ['dividend_amount', 'split_coefficient\r'])
 my_data_new.drop(0, axis = 0, inplace = True)
@@ -54,168 +78,159 @@ my_data_new.drop(101, axis = 0, inplace = True)
 my_data_new = my_data_new.iloc[::-1]        # reverse all rows
 my_data_new.reset_index(drop = True, inplace = True)
 
-# Converting the Data type of each data item like close, high, low etc from "Object" to "numeric"
 for i in range(0, len(my_data_new.columns)):
     my_data_new.iloc[:,i] = pd.to_numeric(my_data_new.iloc[:,i], errors='ignore')
 
 
-# Defining the Theme for Plotting work
+# Plotting work
 plt.style.use('seaborn')
 
-# Important Functions
-# 1) this returns the mean absolute error b/w the predicted and the true value 
 def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
-# 2) this function plots the moving average and the Required Plot
-# accepts a pandas data frame named series => Required Plot
-# accepts window size for rolling mean
-# accepts name for the png figure to be save
-def plot_moving_average(series, window, my_name):
+def plot_moving_average(series, window, my_name, plot_intervals=False, scale=1.96):
 
-    # creating a DataFrame for storing the rolling mean's
     rolling_mean = series.rolling(window=window).mean()
 
-    # Plotting the Rolling Mean Data
     plt.figure(figsize=(17,8))
     plt.title('Moving average\n window size = {}'.format(window))
     plt.plot(rolling_mean, 'g', label='Rolling mean trend')
-   
-           
+
+    #Plot confidence intervals for smoothed values
+    if plot_intervals:
+        mae = mean_absolute_error(series[window:], rolling_mean[window:])
+        deviation = np.std(series[window:] - rolling_mean[window:])
+        lower_bound = rolling_mean - (mae + scale * deviation)
+        upper_bound = rolling_mean + (mae + scale * deviation)
+        plt.plot(upper_bound, 'r--', label='Upper bound / Lower bound')
+        plt.plot(lower_bound, 'r--')
+
     plt.plot(series[window:], label='Actual values')
-    plt.legend(loc='best')  
+    plt.legend(loc='best')
     plt.grid(True)
     plt.xticks(rotation = 90)
 
-    file_name = "public/fig_prediction_" + my_name + '.png'
-    plt.savefig(file_name)
-    
-    # return the rolling mean DataFrame for the Current Dynamic(close, high, low etc) 
-    # so as to complete the prediction process
+    file_name = "fig_prediction" + my_name + ".png"
+    plt.savefig("public/fig_prediction" + my_name + ".png")
+    #plt.show()
+
     return rolling_mean
 
-# 3) Prediction function for the Market Dynamic
-# We pass last(x2,y2) and 2nd-last mean(x1, y1) and predict x2 + 1
-# though we can use this for and point but it will increase error chance for values  
+# Smooth by the previous 5 days (by week)
 def my_prediction(x1, y1, x2, y2, X):
     slope = (y2 - y1) / (x2 - x1)
     y = y1 + slope * (X - x1)
     return (y, slope)
 
-# 4) Market Mood Predicting Function
-# uses Slope Value of the last 2 rolling mean values to predict the Up / Down Nature of the Prediction Plot
 def mood(slope):
     if(slope >= 0):
         return "Up"
     else:
         return "Down"
 
-# defining number of Rows and Columns into the DataFrame
-r,c = my_data_new.shape
-
-# result_prediction variable to store final result
 result_prediction = ""
 
-# ===============================   Generating Prediction =========================== 
-window = 5
-pleasePlot = sys.argv[2].split()
-for dynamic in pleasePlot:
-    if(dynamic == 'open'):
-        # Plot the Prediction Plot
-        # plt.figure(figsize=(20, 10))
-        # plt.plot(my_data_new.timestamp, my_data_new.open)
-        # plt.savefig('public/fig_opening_price.png')
-        
-        # Calculating the Prediction Value for Next Day
-        rolling_mean = plot_moving_average(my_data_new.open, window, "opening")
-        y2 = rolling_mean.iloc[-1]
-        x2 = r-1
-        y1 = rolling_mean.iloc[-2]
-        x1 = r-2
-        prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
+# CLOSING PRICE
+plt.figure(figsize=(20, 10))
+plt.plot(my_data_new.timestamp, my_data_new.close)
+plt.savefig('public/fig_closing_price.png')
+rolling_mean = plot_moving_average(my_data_new.close, 5, "closing", True)
 
-        # Adding the Results Obtained i.e. Predicted Value and Mood to "result_prediction"
-        # using "%" as a separator
-        result_prediction += str(prediction) + "%" + mood(slope) + "%"
+r,c = my_data_new.shape
+y2 = rolling_mean.iloc[-1]
+x2 = r-1
+y1 = rolling_mean.iloc[-2]
+x1 = r-2
 
-    elif(dynamic == 'close'):
-        # plt.figure(figsize=(20, 10))
-        # plt.plot(my_data_new.timestamp, my_data_new.close)
-        # plt.savefig('public/fig_closing_price.png')
+prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
 
-        rolling_mean = plot_moving_average(my_data_new.close, window, "closing")
-        y2 = rolling_mean.iloc[-1]
-        x2 = r-1
-        y1 = rolling_mean.iloc[-2]
-        x1 = r-2
-        prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
-        # Adding the Results Obtained i.e. Predicted Value and Mood to "result_prediction"
-        # using "%" as a separator
-        result_prediction += str(prediction) + "%" + mood(slope) + "%"
-        
-    elif(dynamic == 'high'):
-        # plt.figure(figsize=(20, 10))
-        # plt.plot(my_data_new.timestamp, my_data_new.high)
-        # plt.savefig('public/fig_high_price.png')
+# adding the results to result_prediction
+result_prediction += str(prediction) + "%" + mood(slope) + "%"
 
-        rolling_mean = plot_moving_average(my_data_new.high, window, "high")
-        y2 = rolling_mean.iloc[-1]
-        x2 = r-1
-        y1 = rolling_mean.iloc[-2]
-        x1 = r-2
-        prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
+# OPENING PRICE
+plt.figure(figsize=(20, 10))
+plt.plot(my_data_new.timestamp, my_data_new.open)
+plt.savefig('public/fig_opening_price.png')
+rolling_mean = plot_moving_average(my_data_new.open, 5, "opening", True)
 
-        # Adding the Results Obtained i.e. Predicted Value and Mood to "result_prediction"
-        # using "%" as a separator
-        result_prediction += str(prediction) + "%" + mood(slope) + "%"
-        
-    elif(dynamic == 'low'):
-        # plt.figure(figsize=(20, 10))
-        # plt.plot(my_data_new.timestamp, my_data_new.low)
-        # plt.savefig('public/fig_low_price.png')
+r,c = my_data_new.shape
+y2 = rolling_mean.iloc[-1]
+x2 = r-1
+y1 = rolling_mean.iloc[-2]
+x1 = r-2
 
-        rolling_mean = plot_moving_average(my_data_new.low, window, "low")
-        y2 = rolling_mean.iloc[-1]
-        x2 = r-1
-        y1 = rolling_mean.iloc[-2]
-        x1 = r-2
-        prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
+prediction_tmp, slope = my_prediction(x1, y1, x2, y2, x2+1)
 
-        # adding the results to result_prediction
-        result_prediction += str(prediction) + "%" + mood(slope) + "%"
-        
-    elif(dynamic == 'adjusted_close'):
-        # plt.figure(figsize=(20, 10))
-        # plt.plot(my_data_new.timestamp, my_data_new.adjusted_close)
-        # plt.savefig('public/fig_adjusted_close_price.png')
+# adding the results to result_prediction
+result_prediction += str(prediction) + "%" + mood(slope) + "%"
 
-        rolling_mean = plot_moving_average(my_data_new.adjusted_close, window, "adjusted_close")
-        y2 = rolling_mean.iloc[-1]
-        x2 = r-1
-        y1 = rolling_mean.iloc[-2]
-        x1 = r-2
-        prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
+# HIGH
+plt.figure(figsize=(20, 10))
+plt.plot(my_data_new.timestamp, my_data_new.high)
+plt.savefig('public/fig_high_price.png')
+rolling_mean = plot_moving_average(my_data_new.high, 5, "high", True)
 
-        # adding the results to result_prediction
-        result_prediction += str(prediction) + "%" + mood(slope) + "%"
+r,c = my_data_new.shape
+y2 = rolling_mean.iloc[-1]
+x2 = r-1
+y1 = rolling_mean.iloc[-2]
+x1 = r-2
 
-    elif(dynamic == 'volume'):
-        # plt.figure(figsize=(20, 10))
-        # plt.plot(my_data_new.timestamp, my_data_new.volume)
-        # plt.savefig('public/fig_volume.png')
+prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
 
-        rolling_mean = plot_moving_average(my_data_new.volume, window, "volume")
-        y2 = rolling_mean.iloc[-1]
-        x2 = r-1
-        y1 = rolling_mean.iloc[-2]
-        x1 = r-2
-        prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
+# adding the results to result_prediction
+result_prediction += str(prediction) + "%" + mood(slope) + "%"
 
-        # adding the results to result_prediction
-        result_prediction += str(prediction) + "%" + mood(slope) + "%"
+# LOW
+plt.figure(figsize=(20, 10))
+plt.plot(my_data_new.timestamp, my_data_new.low)
+plt.savefig('public/fig_low_price.png')
+rolling_mean = plot_moving_average(my_data_new.low, 5, "closing", True)
 
-    else:
-        print("No Market Dynamic Found to Plot")
+r,c = my_data_new.shape
+y2 = rolling_mean.iloc[-1]
+x2 = r-1
+y1 = rolling_mean.iloc[-2]
+x1 = r-2
+
+prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
+
+# adding the results to result_prediction
+result_prediction += str(prediction) + "%" + mood(slope) + "%"
+
+# ADJUSTED PRICE
+plt.figure(figsize=(20, 10))
+plt.plot(my_data_new.timestamp, my_data_new.adjusted_close)
+plt.savefig('public/fig_adjusted_close_price.png')
+rolling_mean = plot_moving_average(my_data_new.close, 5, "adjusted_close", True)
+
+r,c = my_data_new.shape
+y2 = rolling_mean.iloc[-1]
+x2 = r-1
+y1 = rolling_mean.iloc[-2]
+x1 = r-2
+
+prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
+
+# adding the results to result_prediction
+result_prediction += str(prediction) + "%" + mood(slope) + "%"
+
+# VOLUME
+plt.figure(figsize=(20, 10))
+plt.plot(my_data_new.timestamp, my_data_new.volume)
+plt.savefig('public/fig_volume.png')
+rolling_mean = plot_moving_average(my_data_new.volume, 5, "volume", True)
+
+r,c = my_data_new.shape
+y2 = rolling_mean.iloc[-1]
+x2 = r-1
+y1 = rolling_mean.iloc[-2]
+x1 = r-2
+
+prediction, slope = my_prediction(x1, y1, x2, y2, x2+1)
+
+# adding the results to result_prediction
+result_prediction += str(prediction) + "%" + mood(slope)
 
 # returning the results
 print(result_prediction)
